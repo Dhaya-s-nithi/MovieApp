ba367cb527130f7851d488bf3c3fd85dd353b332
Dhayanithi S <74674127+Dhaya-s-nithi@users.noreply.github.com>
2022-01-21 21:23:40 +0530

    Delete src directory

--- src/main/java/com/codepark/moviedemoapp/network/Repository.kt
+++
@@ -1,7 +0,0 @@
-package com.codepark.moviedemoapp.network
-
-import retrofit2.Callback
-
-interface Repository {
-    fun getMovieList(searchQuery: String, callBackListener: Callback<List<MovieListResponse>>)
-}
